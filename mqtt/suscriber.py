34331b6dde5364b20bae1e5652515fd0f23dd747
alvarodev-lc <alvaro.lopez19997@gmail.com>
2021-10-28 14:59:45 +0200

    Connected dashboard with suscriber

--- mqtt/suscriber.py
+++ mqtt/suscriber.py
@@ -1,7 +1,8 @@
 import random
 
 from paho.mqtt import client as mqtt_client
-
+from tkinter import *
+from PIL import Image, ImageTk
 
 broker = 'broker.emqx.io'
 port = 1883
@@ -29,6 +30,8 @@ def connect_mqtt() -> mqtt_client:
 
 def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
+        if msg.topic == "sensors/temperature":
+            update_dashboard_temp(msg)
         print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
 
     client.subscribe(temp_topic)
@@ -36,11 +39,44 @@ def subscribe(client: mqtt_client):
     client.on_message = on_message
 
 
-def run():
-    client = connect_mqtt()
-    subscribe(client)
-    client.loop_forever()
+def update_dashboard_temp(msg):
+    temp_label.config(text=msg.payload.decode() + " °C",
+                      fg="black")
 
 
-if __name__ == '__main__':
-    run()
+window = Tk()
+window.title("MQTT Dashboard")
+window.geometry("720x720")
+window.configure(bg="white")
+
+canvas = Canvas(window, bg="white", width=720, height=720)
+# To erase the border of the canvas
+canvas.config(highlightthickness=0)
+canvas.place(x=0, y=0)
+img = PhotoImage(file="../images/logo_test.png")
+canvas.create_image(0, 0, anchor=NW, image=img)
+
+canvas2 = Canvas(window, bg="white", width=100, height=100)
+# To erase the border of the canvas
+canvas2.config(highlightthickness=0)
+canvas2.place(x=50, y=165)
+img2 = Image.open("../images/temp.png")
+resized_img = img2.resize((100, 100), Image.ANTIALIAS)
+img2 = ImageTk.PhotoImage(resized_img)
+canvas2.create_image(0, 0, anchor=NW, image=img2)
+
+# Create Label
+temp_label = Label(window,
+                   text=" °C",
+                   bg="white",
+                   fg="black",
+                   font=("Helvetica", 32))
+
+temp_label.place(x=180, y=190)
+
+client = connect_mqtt()
+subscribe(client)
+client.loop_start()
+
+window.mainloop()
+client.loop_stop()
