4049057c07cd56d6fd72ec1d864b61a134fe78fd
Alay1 <a.ayala.db@gmail.com>
2021-11-17 19:36:07 +0000

    Include initialization script

---
+++ beaglebone_client/initScript.sh
@@ -0,0 +1,9 @@
+date --set 2021-11-17
+date --set 19:34:45
+
+./node 1 </dev/null &>/dev/null &
+./node 2 </dev/null &>/dev/null &
+./node 3 </dev/null &>/dev/null &
+./node 4 </dev/null &>/dev/null &
+./node 5 </dev/null &>/dev/null &
+
